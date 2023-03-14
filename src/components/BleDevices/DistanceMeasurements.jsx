fb1b699398807f8c87e37c8e7ec9914afb3dee05
Sven <sven.hoyer@hotmail.com>
2023-03-14 15:26:43 +0100

    add state for running check

--- src/components/BleDevices/DistanceMeasurements.jsx
+++ src/components/BleDevices/DistanceMeasurements.jsx
@@ -43,7 +43,6 @@ export default function DistanceMeasurements(props) {
     }
 
     const stopMeasurements = () => {
-        
         try {
             ws.send(
                 JSON.stringify( {
